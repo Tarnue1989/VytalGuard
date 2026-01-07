fa2f7d7935128e2124770cac372104f57f769ffc
Tarnue1989 <dtarnue1989@hotmail.com>
2026-01-07 17:07:23 -0500

    Clean

--- frontend/js/dashboard OLD/index.js
+++
@@ -1,7 +0,0 @@
-// 📁 frontend/js/dashboard/index.js
-import { initDashboardSession } from "./dashboard-handler.js";
-import "./dashboard-init.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initDashboardSession();
-});
